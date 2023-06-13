424fb5be974660d47f0ad2cf8ea76ee2cb31edcd
Imran khan <ik1155486@gmail.com>
2023-06-13 10:36:58 -0700

    upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    Strings: ["Fronted Developer", "Youtuber", "Blogger"],
+    typedSpeed: 75,
+    backSpeed: 75,
+    backDelay: 1000,
+    loop: true
+})
